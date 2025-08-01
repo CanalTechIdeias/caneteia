b7d1450b86464dd9102070081dbdbb05898fd766
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-08-01 13:52:22 -0300

    Update main layout

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from caneteia!")
+from nicegui import ui
 
+with ui.grid(columns=16).classes('w-full h-full gap-0'):
+    with ui.column().classes('col-span-3 h-full border p-1'):
+        with ui.row().classes('w-full h-full'):
+            with ui.column().classes('w-full h-full border'):
+                ui.label('1')
+        ui.separator()
+        with ui.row().classes('w-full h-full'):
+            with ui.column().classes('w-full h-full border'):
+                ui.label('2')
+    with ui.column().classes('col-span-10 h-full border p-1'):
+        ui.editor().classes('w-full')
+    with ui.column().classes('col-span-3 h-full border p-1'):
+        pass
+        
 
-if __name__ == "__main__":
-    main()
+ui.run()
